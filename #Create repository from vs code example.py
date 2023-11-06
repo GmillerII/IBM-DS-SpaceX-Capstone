db8936840d142d7a82842082c2d154eb044fe4e7
Grant <GMII15@hotmail.com>
2023-11-06 13:28:54 -0500

    First modification after clicking commit.

--- #Create repository from vs code example.py
+++ #Create repository from vs code example.py
@@ -1,1 +1,3 @@
-#Create repository from vs code example
+#Create repository from vs code example:
+#Create new python file and save it in project folder
+#Click source control and link up to git or github
