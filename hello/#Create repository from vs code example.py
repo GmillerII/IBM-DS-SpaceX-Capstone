5409a778bb149af852765c368f845ba567bf2e2a
Grant Miller <gmii15@hotmail.com>
2024-12-17 14:00:00 -0500

    1

---
+++ hello/#Create repository from vs code example.py
@@ -0,0 +1,3 @@
+#Create repository from vs code example:
+#Create new python file and save it in project folder
+#Click source control and link up to git or github
---
+++ hello/Mod 3 Quiz.py
@@ -0,0 +1,4 @@
+def add(a, b):
+    return(a + b)
+# Write your code below and press Shift+Enter to execute
+print(add(1,2))
